743887eeb7fcd68c2dc134cfb4b48ad891a0bc40
Sweets <Sweets@users.noreply.github.com>
2020-09-07 02:32:13 -0700

    Implement socket writing

--- src/ipc/controller.c
+++ src/ipc/controller.c
@@ -1,3 +1,5 @@
+#include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 #include "controller.h"
@@ -6,14 +8,30 @@
 #include "../util/logging.h"
 
 int prism_controller(int argc, char **argv) {
-    (void)argc;
-    (void)argv;
+    if (argc <= 1)
+        return EXIT_FAILURE;
 
     if (!initialize_socket(CONTROLLER))
         return EXIT_FAILURE;
 
-    write_to_socket("-q", socket_file_descriptor);
-    log_debug("%s", read_from_socket(socket_file_descriptor));
+    // 	private use: U+F0000 - U+FFFFF
+
+    char *buffer = calloc(sizeof(char), 256);
+    for (unsigned int index = 1; index < argc;) {
+        if (strlen(buffer) + strlen(argv[index]) > 256)
+            break;
+
+        strcat(buffer, argv[index++]);
+        buffer[strlen(buffer)] = ' ';
+    }
+    buffer[strlen(buffer)] = '\0';
+
+    write_to_socket(buffer, socket_file_descriptor);
+
+    char *reply = read_from_socket(socket_file_descriptor);
+    if (reply) {
+        printf("prism: %s\n", reply);
+    }
 
     finalize_socket();
 
