9458f4d363e5dba53592af3b13dee318dbf24511
Sweets <sweets@comfi.es>
2022-06-19 10:52:33 -0700

    Move Axabl initialization to manager.m

--- src/prism.m
+++ src/prism.m
@@ -1,23 +1,14 @@
 #import <Foundation/Foundation.h>
 #import <Cocoa/Cocoa.h>
 
-#import "../axabl/AXUIElement.h"
-#import "../axabl/NSWorkspace.h"
+#import "wm/manager.h"
 
 int main(int argc, char **argv) {
+    /* Setup process */
     ProcessSerialNumber psn = {
         .lowLongOfPSN = kCurrentProcess
     };
     TransformProcessType(&psn, kProcessTransformToBackgroundApplication);
 
-    if (!initialize_axui())
-        return EXIT_FAILURE;
-    initialize_nsworkspace();
-
-    CFRunLoopRun();
-
-    finalize_nsworkspace();
-    finalize_axui();
-
-    return EXIT_SUCCESS;
+    return prism();
 }
