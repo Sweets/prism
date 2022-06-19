a5f7cf8830143e80f44a38f9a89e6a6e6d3b716c
Sweets <sweets@comfi.es>
2022-06-19 11:10:55 -0700

    Collect application windows when launched

--- src/handlers/application.m
+++ src/handlers/application.m
@@ -7,6 +7,7 @@
 
 #import "application.h"
 #import "handlers.h"
+#import "window.h"
 
 void application_launched(NSRunningApplication *application, id process_identifier) {
     AXUIElementRef ax_element = axui_create_application_element(process_identifier);
@@ -14,6 +15,14 @@ void application_launched(NSRunningApplication *application, id process_identifi
 
     for (NSString *notification in window_event_handlers)
         axui_install_observer(ax_element, (CFStringRef)notification, generic_observer_callback);
+
+    NSArray *windows = (NSArray *)axui_get_attribute(ax_element, kAXWindowsAttribute);
+
+    AXUIElementRef element_ref;
+    for (id window_ax_element in windows) {
+        element_ref = (AXUIElementRef)window_ax_element;
+        window_created(axui_get_window_id(element_ref), element_ref);
+    }
 
     [managed_ax_elements setObject:encoded_ax_element forKey:process_identifier];
 }
